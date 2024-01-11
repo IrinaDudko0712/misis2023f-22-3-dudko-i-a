75a7e818f5e560f374884495b7fc9fe6645fa7ca
IrinaDudko0712 <113918542+IrinaDudko0712@users.noreply.github.com>
2024-01-11 23:48:31 +0300

    Create 0050a.cpp

---
+++ prj.codeforces/0050a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+ 
+int main(){
+    int n, m, res;
+    cin >> n >> m;
+    res = floor((n*m)/2);
+    cout << res << endl;
+    return 0;
+}
